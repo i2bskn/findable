dfd0f67983c455aec21b6d8105438a952b997ee2
i2bskn <i2bskn@gmail.com>
2015-05-06 23:51:37 +0900

    install simplecov

--- findable.gemspec
+++ findable.gemspec
@@ -25,8 +25,10 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec-rails"
+  spec.add_development_dependency "rspec"
+  spec.add_development_dependency "activerecord"
   spec.add_development_dependency "pry"
   spec.add_development_dependency "coveralls"
+  spec.add_development_dependency "simplecov"
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
-require "findable"
+require "simplecov"
 require "coveralls"
 Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter "spec"
+  add_filter ".bundle"
+  add_filter "example"
+end
+
+require "findable"
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
